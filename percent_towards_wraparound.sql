b33c51a05e86c74f69f4d712b259ed82c2faa0df
nikita <98955280+nikitos-bertone@users.noreply.github.com>
2023-11-11 23:41:36 +0300

    Add files via upload

---
+++ percent_towards_wraparound.sql
@@ -0,0 +1,15 @@
+WITH max_age AS (
+    SELECT 2000000000 as max_old_xid
+        , setting AS autovacuum_freeze_max_age
+        FROM pg_catalog.pg_settings
+        WHERE name = 'autovacuum_freeze_max_age' )
+, per_database_stats AS (
+    SELECT datname
+        , m.max_old_xid::int
+        , m.autovacuum_freeze_max_age::int
+        , age(d.datfrozenxid) AS oldest_current_xid
+    FROM pg_catalog.pg_database d
+    JOIN max_age m ON (true)
+    WHERE d.datallowconn and not d.datistemplate)
+SELECT max(ROUND(100*(oldest_current_xid/max_old_xid::float))) AS percent_towards_wraparound
+FROM per_database_stats;
